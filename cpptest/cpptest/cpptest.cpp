224aace875d0ba9c796264b512b6306b98386006
jiaheng.song <song8713@qq.com>
2022-03-26 23:59:03 +0800

    提交cpp hack示例

---
+++ cpptest/cpptest/cpptest.cpp
@@ -0,0 +1,11 @@
+﻿// cpptest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+const auto p = "fjagsfhgjdfhguodfhfdgfdgfdgdfgdfgsdgfdgfjagsfhgjdfhguodfhfdgfdgfdgdfgdfgsdgfdgfjagsfhgjdfhguodfhfdgfdgfdgdfgdfgsdgfdgfjagsfhgjdfhguodfhfdgfdgfdgdfgdfgsdgfdgfjagsfhgjdfhguodfhfdgfdgfdgdfgdfgsdgfdgfjagsfhgjdfhguodfhfdgfdgfdgdfgdfgsdgfdgfjagsfhgjdfhguodfhfdgfdgfdgdfgdfgsdgfdgfjagsfhgjdfhguodfhfdgfdgfdgdfgdfgsdgfdgfjagsfhgjdfhguodfhfdgfdgfdgdfgdfgsdgfdgfjagsfhgjdfhguodfhfdgfdgfdgdfgdfgsdgfdgfjagsfhgjdfhguodfhfdgfdgfdgdfgdfgsdgfdgfjagsfhgjdfhguodfhfdgfdgfdgdfgdfgsdgfdg";
+
+int main()
+{
+    std::cout << p;
+}
